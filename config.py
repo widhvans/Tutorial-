b156f0e67f47271f63b08071c2fe6a6c728995e8
widhvans <110589162+widhvans@users.noreply.github.com>
2025-06-24 14:51:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 # config.py
-BOT_TOKEN = "7513275210:AAGgRzfVk1_nEaekouSY1kWmp0eqD3pNw-I"  # Replace with your Telegram Bot Token
+BOT_TOKEN = "7320891454:AAHp3AAIZK2RKIkWyYIByB_fSEq9Xuk9-bk"  # Replace with your Telegram Bot Token
 LINK_URL = "https://t.me/joinnowearn"  # Replace with the link you want the bot to send
